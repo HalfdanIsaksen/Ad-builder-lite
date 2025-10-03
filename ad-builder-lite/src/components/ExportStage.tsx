0695b04b263e290d4579501ac81860d4fc011287
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-10-03 11:19:19 +0200

    default layout

--- ad-builder-lite/src/components/ExportStage.tsx
+++ ad-builder-lite/src/components/ExportStage.tsx
@@ -1,4 +1,10 @@
-
+import { exportHTML5Banner, exportJSON } from "../utils/exporters";
 export default function ExportStage() {
-    return (<div>Export Stage</div>);
+    return (
+        <div>
+            <h2>Export Stage</h2>
+            <button onClick={exportHTML5Banner}>Export HTML5 Banner</button>
+            <button onClick={exportJSON}>Export JSON</button>
+        </div>
+    );
 }
